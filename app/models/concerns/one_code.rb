0302b0b6e478eac77aa285f6864dcd79904adca9
Jenny Kim <kimj1905@gmail.com>
2021-07-18 22:19:01 -0400

    commit tree bbd5b986c01e277170d50a032d941f25dc8b2e8e

--- app/models/concerns/one_code.rb
+++ app/models/concerns/one_code.rb
@@ -1,14 +1,23 @@
 module OneCode
     module InstanceMethod
-        def openings(first_date, last_date)
-            Listing.joins(:reservations).where.not("checkin BETWEEN ? AND ?", first_date, last_date) &
-            Listing.joins(:reservations).where.not("checkout BETWEEN ? AND ?", first_date, last_date)
-            # Listing.joins(:reservations).where.not("? <= checkout <= ?", first_date, last_date)
+        def city_neighborhood_openings(first_date, last_date)
+            # Listing.joins(:reservations).where.not("checkin BETWEEN ? AND ?", first_date, last_date) &
+            # Listing.joins(:reservations).where.not("checkout BETWEEN ? AND ?", first_date, last_date)
+            Listing.joins(:reservations).where.not("checkin <= ? AND checkout >= ?", first_date, last_date).uniq
         end
     end
 
     module ClassMethods
         def highest_ratio_res_to_listings
+            # Listing.all.count do |listing|
+            counting = Listing.all.count do |listing|
+                listing.neighborhood.city.listings
+            end
+
+            # counting
+            byebug
+
+
         end
 
         def most_res
